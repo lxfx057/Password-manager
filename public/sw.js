520bc5f239ef5d9021b8fad1efab6b023936f705
Luca Finaldi <lucafinaldi3@gmail.com>
2026-08-14 16:31:40 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+{
+  "name": "BlueVault",
+  "short_name": "BlueVault",
+  "description": "Cassaforte locale per password",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#07152f",
+  "theme_color": "#07152f",
+  "lang": "it",
+  "icons": []
+}
